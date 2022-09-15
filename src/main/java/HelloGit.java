ed725565bd1948d5f415d8703cbd4527aa289195
小丝_Lemon <104551297+LemonMiaocat@users.noreply.github.com>
2022-09-15 21:18:10 +0800

    线上第一次更改代码

--- src/main/java/HelloGit.java
+++ src/main/java/HelloGit.java
@@ -4,6 +4,7 @@ public class HelloGit {
         System.out.println("第一次热修改");
         System.out.println("第二次修改！！！！！！");
         System.out.println("第二次热修改");
+        System.out.println("在github上修改代码");
     }
 
 }
