d7fc5a5033552ea5dec7431ee5d05246fe4e8a2c
Xiaosi_lemon <Xiaosi_lemon@qq.com>
2022-09-15 20:36:28 +0800

    第二次热修改提交

--- src/main/java/HelloGit.java
+++ src/main/java/HelloGit.java
@@ -2,6 +2,7 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("你好呀git！");
         System.out.println("第一次热修改");
+        System.out.println("第二次热修改");
     }
 
 }
